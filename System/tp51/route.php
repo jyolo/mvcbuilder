1a289ea581618161ae6263cc12f1bce5b7b5896a
jyolo <438608613@qq.com>
2018-03-16 16:48:10 +0800

    删除外面的system

--- System/tp51/route.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use MvcBuilder\System\tp51\MvcBuilderController;
-Route::rule('mvcbuilder/:action', function ($action,MvcBuilderController $builderController){
-    return $builderController->$action();
-});
-
