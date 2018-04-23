83330f394b7ec45a0d51671b54e05cdb10704938
jyolo <438608613@qq.com>
2018-04-23 17:43:11 +0800

    优化结构

--- Driver/Thinkcmf/System/route.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use MvcBuilder\Driver\ThinkCmf\System\MvcBuilderController;
-Route::rule('mvcbuilder/:action', function ($action,MvcBuilderController $builderController){
-    return $builderController->$action();
-});
-
