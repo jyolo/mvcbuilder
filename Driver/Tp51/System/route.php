e55b9eac476afc62a32528c633c0336f57814f58
jyolo <438608613@qq.com>
2018-04-23 17:42:11 +0800

    优化结构

--- Driver/Tp51/System/route.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use MvcBuilder\Driver\Tp51\System\MvcBuilderController;
-Route::rule('mvcbuilder/:action', function ($action,MvcBuilderController $builderController){
-    return $builderController->$action();
-});
-
